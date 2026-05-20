a91bc551a60cb4a0886df7848b63036b21b102ac
mrp <pro100skm@yandex.ru>
2026-05-20 23:16:22 +0300

    feat: return back 150k. and open udp

--- mainnet/start-node.sh
+++ mainnet/start-node.sh
@@ -81,7 +81,7 @@ args=(
 if echo "${ENABLE_RPC}" | grep -iq "true"; then
     args+=(
         --rpc
-        --rpc-gascap "250000000"
+        --rpc-gascap "150000000"
         --rpcaddr "${RPC_ADDR}"
         --rpcport "${RPC_PORT}"
         --rpcapi "${RPC_API}"
