ae6d16a3730f340355ff97d7500331a2cd3e0d4b
rmn <pro100skm@yandex.ru>
2026-05-24 11:51:18 +0300

    Revert "gas cap remove"
    This reverts commit e392c34f21c0fbc6294e03fd934858bf81b132e9.

--- mainnet/start-node.sh
+++ mainnet/start-node.sh
@@ -94,7 +94,7 @@ if [[ "${ENABLE_RPC}" == "true" ]]; then
         --http-api "${API}"
         --http-corsdomain "${ALLOWED_ORIGINS}"
         --http-vhosts "${RPC_VHOSTS}"
-#        --rpc-gascap "300000000"
+        --rpc-gascap "300000000"
     )
 else
     # When not "true", explicitly disable RPC to avoid unintended exposure
